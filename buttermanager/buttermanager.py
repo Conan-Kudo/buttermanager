c0f08a485f97a6a0dbc26966d7e88415d6ad0647
Neal Gompa <ngompa13@gmail.com>
2020-12-30 21:14:24 -0500

    Generate a proper entry point using setuptools
    We need this in order for the application to install and run
    when properly packaged.

--- buttermanager/buttermanager.py
+++ buttermanager/buttermanager.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-#
 # -*- coding: utf-8 -*-
 #
 # Copyright 2018-2019 Eloy García Almadén <eloy.garcia.pca@gmail.com>
@@ -1144,7 +1142,10 @@ class ButtermanagerMainWindow(QMainWindow):
         info_dialog.show()
 
 
-if __name__ == '__main__':
+def main():
+    """Main wrapper for starting the program
+
+    """
     # Creating application instance
     application = QApplication(sys.argv)
     # Creating main window instance
